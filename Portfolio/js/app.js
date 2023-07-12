2134e576b4f10ca6bb043b53b897d6885671c468
Prasurjyasarma <82230151+Prasurjyasarma@users.noreply.github.com>
2023-07-12 23:04:54 +0530

    Add files via upload

---
+++ Portfolio/js/app.js
@@ -0,0 +1,13 @@
+window.addEventListener('scroll', e => {
+	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
+})
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+	wrapper: '.wrapper',
+	content: '.content'
+})
+
+var loader= document.getElementById("perloader");
+window.addEventListener("load",function(){
+	loader.style.display="none";
+})
